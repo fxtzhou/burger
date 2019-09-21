471b5f386df61ad6dd52eeac1d8bd739aa93a06a
Fiona Zhou <fxtzhou@yahoo.com>
2019-09-21 00:09:19 -0700

    working on handlebars file

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("double double cheeseburger", FALSE);
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("double shack burger", FALSE);
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("spicy chicken sandwich", FALSE);
+
